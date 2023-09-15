88821202553eeafa3ff79e633ea95f9251ef1fc2
Abhishek <Abhishek@gmail.com>
2023-09-15 14:27:41 +0530

    update session

--- src/main/java/EmployeeController/LoginServlet.java
+++ src/main/java/EmployeeController/LoginServlet.java
@@ -52,34 +52,31 @@ public class LoginServlet extends HttpServlet {
 			Employee emp = service.validateEmployee(userName, password);
 			System.out.println("userName::" + userName + " Password::" + password);
 			if (emp != null) {
-				
+
 				System.out.println("Emp not null");
-				if(!emp.isAdmine())
-				{
-					HttpSession ses =request.getSession();
+				if (!emp.isAdmine()) {
+					HttpSession ses = request.getSession();
 					System.out.println("not a admin");
-					 ses=request.getSession();
+					ses = request.getSession();
 					ses.setAttribute("emp", emp);
 					ses.setAttribute("isAdmin", false);
-					 request.setAttribute("login", "login successful");
-					RequestDispatcher rd=request.getRequestDispatcher("Employee.jsp");
-					 rd.forward(request, response);
-				}
-				else
-				{
-					HttpSession ses =request.getSession();
-					System.out.println("session in login::"+ses);
-					List<Employee> empList=service.getListOfEmployee();
-					 ses=request.getSession();
+					request.setAttribute("login", "login successful");
+					RequestDispatcher rd = request.getRequestDispatcher("Employee.jsp");
+					rd.forward(request, response);
+				} else {
+					HttpSession ses = request.getSession();
+					System.out.println("session in login::" + ses);
+					List<Employee> empList = service.getListOfEmployee();
+					ses = request.getSession();
 					ses.setAttribute("empList", empList);
 					ses.setAttribute("isAdmin", true);
 					request.setAttribute("login", "login successful");
-					RequestDispatcher rd=request.getRequestDispatcher("admin.jsp");
+					RequestDispatcher rd = request.getRequestDispatcher("admin.jsp");
 					rd.forward(request, response);
 				}
 			} else {
 				request.setAttribute("msg", "invalid credentials");
-				RequestDispatcher rd=request.getRequestDispatcher("login.jsp");
+				RequestDispatcher rd = request.getRequestDispatcher("login.jsp");
 				rd.include(request, response);
 				System.out.println("Emp null");
 			}
